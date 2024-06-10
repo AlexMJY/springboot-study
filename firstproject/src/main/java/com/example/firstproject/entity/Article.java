6bfbceaa20a54dde1125aece9630f0762a304f05
Alex-MJY <jmoon0227@gmail.com>
2024-06-11 01:00:45 +0900

    SQL query log 확인하기

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,8 @@ import lombok.ToString;
 @Data  // Getter, Setter
 public class Article {
     @Id // Entity의 대표값 지정 (PK)
-    @GeneratedValue // 자동 생성 기능 추가 (MySQL의 Auto Increment)
+    // @GeneratedValue // 자동 생성 기능 추가 (MySQL의 Auto Increment)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 데이터를 생성할 때마다 DB가 id 자동 생성
     private Long id;
 
     @Column  // title 필드 선언, DB table의 title 열과 연결됨

b697bf52264a4284e18d344d3d77191d75de5197
Alex-MJY <jmoon0227@gmail.com>
2024-06-15 14:51:25 +0900

    delete .gitignore

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -5,3 +5,7 @@
 INSERT INTO article(title, content) VALUES ('aaaa', '1111');
 INSERT INTO article(title, content) VALUES ('bbbb', '2222');
 INSERT INTO article(title, content) VALUES ('cccc', '3333');
+
+INSERT INTO coffee(name, price) VALUES ('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES ('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES ('카페 모카', '5500');

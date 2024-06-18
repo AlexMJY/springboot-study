2e14de6763e89c77c3b8fd78d58b8ae0d6210bea
AlexMJY <jmoon0227@gmail.com>
2024-06-18 14:38:06 +0900

    14.3 댓글 리파지터리 만들기

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT INTO article(title, content) VALUES ('cccc', '3333');
 
 INSERT INTO article(title, content) VALUES ('당신의 인생 영화는?', '댓글 고');
 INSERT INTO article(title, content) VALUES ('당신의 소울푸드는?', '댓글 고고');
-INSERT INTO article(title, content) VALUES ('최고의 축구선수는?', '댓글 궈궈궈');
+INSERT INTO article(title, content) VALUES ('당신의 취미는?', '댓글 고고고');
 
 
 INSERT INTO comment(article_id, nickname, body) VALUES (4, 'Park', '굿 윌 헌팅');
@@ -20,9 +20,9 @@ INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Park', '치킨');
 INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Kim', '샤브샤브');
 INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Choi', '초밥');
 
-INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Park', '메시');
-INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Kim', '호날두');
-INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Choi', '음바페');
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Park', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Kim', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Choi', '독서');
 
 
 INSERT INTO coffee(name, price) VALUES ('아메리카노', '4500');
